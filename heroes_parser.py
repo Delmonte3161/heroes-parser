f6d470016eb6fec51fb092733f80c1ed29f24096
ds3161 <dsaus@cerner.com>
2015-09-21 11:57:49 -0500

    Changed to use auto increment ids

--- heroes_parser.py
+++ heroes_parser.py
@@ -5,24 +5,26 @@ import os
 import psycopg2
 import ConfigParser
 
-def InsertMatchRecord(conn, match_id, key, datestring, map, winning_team_id):
-    print "InsertMatchRecord: " + str(match_id) + " : " + str(key) + " : " + str(datestring) + " : " + str(
+
+def insert_match_record(conn, key, datestring, map, winning_team_id):
+    print "insert_match_record: " + " : " + str(key) + " : " + str(datestring) + " : " + str(
         map) + " : " + str(winning_team_id)
     cursor = conn.cursor()
     query = "select id from match where key = '" + key + "';"
     cursor.execute(query)
     rows = cursor.fetchall()
     if len(rows) == 0:
-        query = "insert into match (id, key, date, type, map, winning_team_id) VALUES (%s, %s, to_timestamp(%s), 'unknown', %s, %s);"
-        data = (match_id, key, datestring, map, winning_team_id)
+        query = "insert into match (key, date, type, map, winning_team_id) VALUES (%s, to_timestamp(%s), 'unknown', %s, %s) returning id;"
+        data = (key, datestring, map, winning_team_id)
         cursor.execute(query, data)
-        return 1
+        return cursor.fetchone()[0]
     else:
         print "Match record already exists"
         return 0
 
-def InsertPlayerRecord(conn, player_id, player_name):
-    print "InsertPlayerRecord: " + str(player_id) + " : " + str(player_name)
+
+def insert_player_record(conn, player_id, player_name):
+    print "insert_player_record: " + str(player_id) + " : " + str(player_name)
     cursor = conn.cursor()
     query = "select id from player where id = " + str(player_id) + ";"
     cursor.execute(query)
@@ -35,15 +37,17 @@ def InsertPlayerRecord(conn, player_id, player_name):
         print "Player record already exists"
 
 
-def InsertMatchPlayerRelRecord(conn, id, match_id, team_id, player_id, hero):
-    print "InsertMatchPlayerRelRecord: " + str(id) + " : " + str(match_id) + " : " + str(team_id) + " : " + str(
+def insert_match_player_relationship_record(conn, match_id, team_id, player_id, hero):
+    print "insert_match_player_relationship_record: " + str(id) + " : " + str(match_id) + " : " + str(
+        team_id) + " : " + str(
         player_id) + " : " + str(hero)
     cursor = conn.cursor()
-    query = "insert into match_player_rel (id, match_id, team_id, player_id, hero) VALUES (%s, %s, %s, %s, %s);"
-    data = (id, match_id, team_id, player_id, hero)
+    query = "insert into match_player_rel (match_id, team_id, player_id, hero) VALUES (%s, %s, %s, %s);"
+    data = (match_id, team_id, player_id, hero)
     cursor.execute(query, data)
 
-def GetDBConnection(config):
+
+def get_db_connection(config):
     host = config.get('Database', 'database.host');
     port = config.get('Database', 'database.port');
     dbname = config.get('Database', 'database.dbname');
@@ -55,22 +59,23 @@ def GetDBConnection(config):
     print "dbname " + dbname
 
     try:
-        conn = psycopg2.connect(host=host, port=port,database=dbname, user=user, password=password)
+        conn = psycopg2.connect(host=host, port=port, database=dbname, user=user, password=password)
         return conn
     except:
         print "Error connecting to the database"
 
-def ProcessReplays(conn, path):
+
+def process_replays(conn, path):
     print "Begin processing replays in path: " + path
     match_id = 0
     match_player_rel_id = 0
     for fn in os.listdir(path):
         print fn
         if fn.__contains__(".StormReplay"):
-            replayFile = open(path + fn)
+            replay_file = open(path + fn)
             items = ["RawReplayDetails"]
-            retval = api.tasks.AnalyzeReplayFile(replayFile, items)
-            replayFile.close()
+            retval = api.tasks.AnalyzeReplayFile(replay_file, items)
+            replay_file.close()
             data_string = json.dumps(retval, skipkeys=False, ensure_ascii=False)
             data = json.loads(data_string.decode('utf-8', 'ignore'))
             details = data["raw"]["details"]
@@ -91,34 +96,36 @@ def ProcessReplays(conn, path):
             if computer_found == 1:
                 continue
 
-            match_inserted = 0
+            match_id = 0
             try:
-                match_inserted = InsertMatchRecord(conn, match_id, key, datestring, details["m_title"], winning_team_id)
+                match_id = insert_match_record(conn, key, datestring, details["m_title"], winning_team_id)
             except:
                 "Error in InsertMatchRecord"
                 continue
 
-            if match_inserted == 1:
+            if match_id > 0:
                 for player in details["m_playerList"]:
                     try:
-                        InsertPlayerRecord(conn, player["m_toon"]["m_id"], player["m_name"])
+                        insert_player_record(conn, player["m_toon"]["m_id"], player["m_name"])
                         match_player_rel_id += 1
-                        InsertMatchPlayerRelRecord(conn, match_player_rel_id, match_id, player["m_teamId"],
-                                                   player["m_toon"]["m_id"], player["m_hero"])
+                        insert_match_player_relationship_record(conn, match_id, player["m_teamId"],
+                                                                player["m_toon"]["m_id"], player["m_hero"])
                     except:
                         "Error in InsertPlayerRecord"
             # transactionally commit the match
             conn.commit()
 
-        #break
+            # break # uncomment to test just one match
+
 
 def main():
     print "Starting main program"
     config = ConfigParser.RawConfigParser()
     config.read('heroes.ini')
     path = config.get('Replays', 'replays.path');
-    conn = GetDBConnection(config)
-    ProcessReplays(conn,path)
+    conn = get_db_connection(config)
+    process_replays(conn, path)
+
 
 if __name__ == "__main__":
     main()
@@ -126,7 +133,7 @@ if __name__ == "__main__":
 
 
 
-#        print data_string
+# print data_string
 
 #        # write the json file
 #        with open('/Users/ds3161/parses/' + fn + ".json", 'w') as outfile:
