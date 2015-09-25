52c7283ce4eb737bd3f56733bf2686e4fd84baed
ds3161 <dsaus@cerner.com>
2015-09-25 09:33:09 -0500

    Added analysis for lists larger than 5

--- comp_analysis.py
+++ comp_analysis.py
@@ -6,20 +6,25 @@ import ConfigParser
 import datetime
 import sys
 import itertools
+from decimal import *
 
 
-def get_comp_data(conn, game_type, player_list):
+def get_comp_data(conn, game_type, player_list, exclude_players):
+    getcontext().prec = 3
     cursor = conn.cursor()
 
     comps = get_comps(conn, player_list)
     for comp in comps:
         include_list = comp[0]
-        exclude_list = comp[1]
+        if exclude_players == 0:
+            exclude_list = []
+        else:
+            exclude_list = comp[1]
+
         result = generate_query_string(include_list, exclude_list, game_type, 1)
         query = cursor.mogrify(result, (tuple(exclude_list),))
         cursor.execute(query)
         wins = cursor.fetchone()[0]
-        print str(include_list) + " Wins: " + str()
         result = generate_query_string(include_list, exclude_list, game_type, 0)
         query = cursor.mogrify(result, (tuple(exclude_list),))
         cursor.execute(query)
@@ -27,9 +32,9 @@ def get_comp_data(conn, game_type, player_list):
 
         percent = 0;
         if total > 0:
-            percent = (wins/total) * 100
+            percent = (Decimal(wins)/Decimal(total)) * 100
 
-        print str(include_list) + " Total: " + str(total) + " Wins: " + str(wins) + " Win rate: " + str(percent)
+        print get_comp_string(include_list) + ": Games (" + str(total) + ") " + str(wins) + "-" + str(total - wins) + " (" + str(percent) + "%)"
 
 
 def generate_query_string(include_list, exclude_list, game_type, add_winning_team_clause):
@@ -53,11 +58,25 @@ def generate_query_string(include_list, exclude_list, game_type, add_winning_tea
         query += " and p.name = '"
         query += name
         query += "') "
-    query += " and m.type = '"
-    query += str(game_type)
-    query += "' ;"
+    if len(game_type) > 0:
+        query += " and m.type = '"
+        query += str(game_type)
+        query += "'"
+    query += ";"
+
     return query
 
+
+def get_comp_string(player_list):
+    display = ""
+    count = 0
+    for x in player_list:
+        if count == 0:
+            display += x
+        else:
+            display += ", " + x
+        count += 1
+    return display
 
 def get_db_connection(config):
     host = config.get('Database', 'database.host');
@@ -80,13 +99,15 @@ def get_db_connection(config):
 def get_comps(conn, player_list):
     comp_list = []
     for i in range(len(player_list)):
+        if i + 1 > 5:
+            # don't do comps larger than 5 players
+            break
         result = itertools.combinations(player_list, i + 1)
         for j in result:
             include_list = set(j)
             exclude_list = set(player_list) - set(j)
             comp = [include_list, exclude_list]
             comp_list.append(comp)
-
     return comp_list
 
 
@@ -99,7 +120,24 @@ def main():
     player_list = config.get('CompAnalysis', 'comp.player_list').split(',')
     game_type = config.get('CompAnalysis', 'comp.game_type')
 
-    get_comp_data(conn, game_type, player_list)
+    print ' '
+    print 'Quick Match (w/exclusions)'
+    get_comp_data(conn, 'Quick Match', player_list,1)
+    print ' '
+    print 'Hero League (w/exclusions)'
+    get_comp_data(conn, 'Hero League', player_list,1)
+    print ' '
+    print 'All Game Types (w/exclusions)'
+    get_comp_data(conn, '', player_list, 1)
+    print ' '
+    print 'Quick Match'
+    get_comp_data(conn, 'Quick Match', player_list,0)
+    print ' '
+    print 'Hero League'
+    get_comp_data(conn, 'Hero League', player_list,0)
+    print ' '
+    print 'All Game Types'
+    get_comp_data(conn, '', player_list, 0)
 
 
 if __name__ == "__main__":
